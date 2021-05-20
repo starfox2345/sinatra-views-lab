72343c363d5fa4eab8a0555dfdf04837f8d960a2
yatsun <yatsunlam25@gmail.com>
2021-05-20 00:41:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
 
+	get '/goodbye' do
+		name = "Joe"
+		ERB.new("<h1>Goodbye #{name}</h1>").result
+	end
+
+	get '/date' do
+		today = Date.today.strftime("%A, %B %d, %Y")
+		ERB.new("<h1>The date is #{today}</h1>").result
+	end
 end
